947f671b5adba284ee32398e20ae76b42f9324d4
Ashley Waudby <ashlinho07@gmail.com>
2023-05-25 10:25:41 +0100

    alert messages

---
+++ src/script.js
@@ -0,0 +1,17 @@
+
+let error = document.querySelector(".error-correct-container");
+let closeIcon = document.querySelector(".close-correct");
+
+function addAlert(event){
+if(colorCell.length == score + 1){
+  error.classList.remove("display")
+  
+  }
+}
+
+function closeBox(event){
+  let closeGreen = document.querySelector(".error-correct-container");
+  closeGreen.classList.add("display")
+}
+
+closeIcon.addEventListener("click", closeBox)
